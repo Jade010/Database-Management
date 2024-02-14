bd5c97bf641d34f970939563a13b75f6fd40be90
Jade A <120065528+Jade010@users.noreply.github.com>
2024-02-13 22:58:06 -0800

    Update ER2Rel.sql

--- Database Design and Modeling/SecondProject/ER2Rel.sql
+++ Database Design and Modeling/SecondProject/ER2Rel.sql
@@ -31,7 +31,7 @@ INSERT INTO restaurant VALUES (3, 'Dukes Seafood', '1111 Fairview Ave N', 'Seatt
 
 -- Question 3
 CREATE TABLE FARMER (
-farmerID INT NOT NULL PRIMARY KEY,
+farmerID INT PRIMARY KEY,
 farmer_fname CHAR(20) NOT NULL,
 farmer_lname CHAR(20) NOT NULL,
 farmer_street CHAR(40) NOT NULL,
@@ -40,38 +40,33 @@ farmer_state CHAR(2) NOT NULL,
 farmer_zip CHAR(5) NOT NULL);
 
 CREATE TABLE COW (
-cowID INT NOT NULL PRIMARY KEY,
+cowID INT PRIMARY KEY,
 cow_name CHAR(20) NOT NULL,
 cow_breed CHAR(20) NOT NULL,
 cow_dob DATE NOT NULL,
 farmerID INT NOT NULL,
 CONSTRAINT fk_farmer_cow FOREIGN KEY (farmerID) REFERENCES FARMER (farmerID));
 
-INSERT INTO farmer (farmerID, farmer_fname, farmer_lname, farmer_street, farmer_city, farmer_state, farmer_zip)
-VALUE (1, 'Jane', 'Doe', '123 made up NE', 'Pullman', 'WA', '99163');
-INSERT INTO farmer (farmerID, farmer_fname, farmer_lname, farmer_street, farmer_city, farmer_state, farmer_zip)
-VALUE (2, 'John', 'Doe', '1111 mystery W', 'Spokane', 'WA', '99173');
-INSERT INTO farmer (farmerID, farmer_fname, farmer_lname, farmer_street, farmer_city, farmer_state, farmer_zip)
-VALUE (3, 'Adam', 'Doe', '1 country DR', 'Tacoma', 'WA', '98405');
-INSERT INTO cow (cowID, cow_name, cow_breed, cow_dob, farmerID)
-VALUE (1, 'Bessy', 'Angus', '05/04/2000', 3);
-INSERT INTO cow (cowID, cow_name, cow_breed, cow_dob, farmerID)
-VALUE (2, 'Bertha', 'Holstein', '08/12/2001', 1);
-INSERT INTO cow (cowID, cow_name, cow_breed, cow_dob, farmerID)
-VALUE (3, 'Lu', 'Highland', '11/09/2003', 2);
+INSERT INTO farmer VALUES (1, 'Jane', 'Doe', '123 made up NE', 'Pullman', 'WA', '99163');
+INSERT INTO farmer VALUES (2, 'John', 'Doe', '1111 mystery W', 'Spokane', 'WA', '99173');
+INSERT INTO farmer VALUES (3, 'Adam', 'Doe', '1 country DR', 'Tacoma', 'WA', '98405');
+
+INSERT INTO cow VALUES (1, 'Bessy', 'Angus', '05/04/2000', 3);
+INSERT INTO cow VALUES (2, 'Bertha', 'Holstein', '08/12/2001', 1);
+INSERT INTO cow VALUES (3, 'Lu', 'Highland', '11/09/2003', 2);
 
 
 -- Question 4
 
 CREATE TABLE AIRCRAFT (
-aircraftID INT NOT NULL PRIMARY KEY,
+aircraftID INT PRIMARY KEY,
 make CHAR(20) NOT NULL,
 model CHAR(20) NOT NULL,
 year CHAR(4) NOT NULL,
 
 
 CREATE TABLE FLIGHT (
-flightID INT NOT NULL PRIMARY KEY,
+flightID INT PRIMARY KEY,
 origin CHAR(5) NOT NULL,
 destination CHAR(5) NOT NULL,
 dep_date DATE NOT NULL,
@@ -81,7 +76,7 @@ aircraftID INT NOT NULL,
 CONSTRAINT fk_aircraft_flight FOREIGN KEY (aircraftID) REFERENCES AIRCRAFT (aircraftID));
 
 CREATE TABLE PASSENGER (
-passengerID INT NOT NULL PRIMARY KEY,
+passengerID INT PRIMARY KEY,
 pass_lname CHAR(20) NOT NULL,
 pass_fname CHAR(20) NOT NULL,
 pass_street CHAR(40) NOT NULL,
@@ -93,22 +88,15 @@ pass_class CHAR(10) NOT NULL,
 flightID INT NOT NULL,
 CONSTRAINT fk_flight_passenger FOREIGN KEY (flightID) REFERENCES FLIGHT (flightID));
 
-INSERT INTO aircraft (aircraftID, make, model) VALUE (111, 'Boeing', '737');
-INSERT INTO aircraft (aircraftID, make, model) VALUE (122, 'Airbus', '303');
-INSERT INTO aircraft (aircraftID, make, model) VALUE (133, 'Lockheed Martin', '156');
+INSERT INTO aircraft VALUE (111, 'Boeing', '737');
+INSERT INTO aircraft VALUE (122, 'Airbus', '303');
+INSERT INTO aircraft VALUE (133, 'Lockheed Martin', '156');
 
+INSERT INTO flight VALUE (211, 'SEA', 'GEG', '01-01-2023', '08:00' , '09:10', 133);
+INSERT INTO flight VALUE (222, 'GEG', 'SEA', '05-04-2023', '06:15', '07:30', 111);
+INSERT INTO flight VALUE (233, 'SEA', 'GEG', '12-17-2023', '07:00', '08:00', 122);
 
-INSERT INTO flight (flightID, origin, destination, dep_date, dep_time, arr_time, aircraftID)
-VALUE (211, 'SEA', 'GEG', '01-01-2023', '08:00' , '09:10', 133);
-INSERT INTO flight (flightID, origin, destination, dep_date, dep_time, arr_time, aircraftID)
-VALUE (222, 'GEG', 'SEA', '05-04-2023', '06:15', '07:30', 111);
-INSERT INTO flight (flightID, origin, destination, dep_date, dep_time, arr_time, aircraftID)
-VALUE (233, 'SEA', 'GEG', '12-17-2023', '07:00', '08:00', 122);
-
-INSERT INTO passenger (passengerID, pass_lname, pass_fname, pass_street, pass_city, pass_state, pass_zip, pass_seatno, pass_class, flightID)
-VALUE (311, 'Johnson', 'Anna', '123 random NE', 'Spokane', 'WA', '99158', '16', 'D', 'Coach', 222);
-INSERT INTO passenger (passengerID, pass_lname, pass_fname, pass_street, pass_city, pass_state, pass_zip, pass_seatno, pass_class, flightID)
-VALUE (322, 'Powell', 'Steve', '544 union ave', 'Tacoma', 'WA', '98405', '2', 'A', 'First', 233);
-INSERT INTO passenger (passengerID, pass_lname, pass_fname, pass_street, pass_city, pass_state, pass_zip, pass_seatno, pass_class, flightID)
-VALUE (333, 'Carson', 'Mary', '1515 wheatland DR', 'Pullman', 'WA', '99163', '7', 'B', 'Business', 211);
+INSERT INTO passenger VALUE (311, 'Johnson', 'Anna', '123 random NE', 'Spokane', 'WA', '99158', '16', 'D', 'Coach', 222);
+INSERT INTO passenger VALUE (322, 'Powell', 'Steve', '544 union ave', 'Tacoma', 'WA', '98405', '2', 'A', 'First', 233);
+INSERT INTO passenger VALUE (333, 'Carson', 'Mary', '1515 wheatland DR', 'Pullman', 'WA', '99163', '7', 'B', 'Business', 211);
 
